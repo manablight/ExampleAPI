b007ed058b7dad49d79feb1a9fddcecb5d8f7397
Cody King <mana18@gmail.com>
2023-12-07 08:09:25 -0800

    add service to query database layer to retrieve movie data.

---
+++ Services/MovieService.cs
@@ -0,0 +1,16 @@
+﻿using ExampleAPI.Models;
+using ExampleAPI.Repositories;
+
+namespace ExampleAPI.Services;
+
+public class MovieService
+{
+    private readonly MovieRepository _movieRepository;
+
+    public MovieService(MovieRepository movieRepository) => _movieRepository = movieRepository;
+
+    public bool Create(Movie movie) => _movieRepository.CreateMovie(movie);
+    public Movie GetMovie(Guid id) => _movieRepository.GetMovie(id);
+    public List<Movie> GetAllMovies() => _movieRepository.GetAllMovies();
+    public bool DeleteMovie(Guid id) => _movieRepository.DeleteMovie(id);
+}
