2476b839a50eeb0a7c6e57a3e948661164333a42
Cody King <mana18@gmail.com>
2023-12-07 08:08:54 -0800

    Add model to represent movie data

---
+++ Models/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace ExampleAPI.Models;
+
+public class Movie
+{
+    public required Guid Id { get; init; }
+    public required string Title { get; set; }
+    public int YearOfRelease { get; set; }
+}
